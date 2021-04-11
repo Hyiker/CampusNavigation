d667098688e3de72fa31f5525b35435a470e2809
FantasticGu <1607876517@qq.com>
2021-04-11 21:44:06 +0800

    add_course_init

--- src/model/logical/courseHub.cc
+++ src/model/logical/courseHub.cc
@@ -1,48 +1,10 @@
 #include "courseHub.h"
 
-Course::Course(string name) {
-    int nums = std::count(name.begin(), name.end(), '-');
-    if (nums == 2) {
-        int separator[2] = {0};
-        separator[0] = name.find('-');
-        separator[1] = name.rfind('-');
-        this->_class = name.substr(0, separator[0] - 1);
-        this->courseName = name.substr(separator[0] + 1, separator[1] - separator[0] - 1);
-        this->time = name.substr(separator[1] + 1);
-    } else if (nums == 1) {
-        int separator = name.find('-');
-        this->_class = name.substr(0, separator - 1);
-        this->courseName = name.substr(separator + 1);
-        this->time = "";
-    }
-}
-
-bool Course::operator==(const Course& courseB) {
-    if (this->time == "" || courseB.time == "") {
-        if (this->_class == courseB._class && this->courseName == courseB.courseName) {
-            return 1;
-        }
-    } else if (this->_class == courseB._class && this->courseName == courseB.courseName && this->time == courseB.time) {
-        return 1;
-    }
-    return 0;
-}
-
 CourseHub::CourseHub() {
 }
 
 CourseHub::~CourseHub() {
 }
-
-/*CourseHub::~CourseHub() {
-    std::ofstream outFile;
-    outFile.open("/mnt/c/Users/16078/Desktop/CampusNavigation/src/model/logical/courseList.csv", std::ios::out);
-    auto it = this->course_map.begin();
-    while (it != course_map.end()) {
-        outFile << it->first->Class << ',' << it->first->courseName << ',' << it->first->time << ',' << it->second;
-        it++;
-    }
-}*/
 
 Id CourseHub::getPhysicalId(std::shared_ptr<Course> course_ptr) {
     return course_map[course_ptr];
