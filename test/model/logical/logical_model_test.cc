2667c071f3c86b54249855830402673ae0049ea2
FantasticGu <1607876517@qq.com>
2021-04-21 13:10:47 +0800

    about finish the model

--- test/model/logical/logical_model_test.cc
+++ test/model/logical/logical_model_test.cc
@@ -1,6 +1,6 @@
+#include "model/logical/logical_model.h"
 #include <gtest/gtest.h>
 #include "error/errors.hpp"
+#include "model/logical/course.h"
 #include "model/model.h"
 #include "model/model_hub.h"
-#include "model/logical/logical_model.h"
-#include "model/logical/course.h"
